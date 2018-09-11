005c35f39564a9bdc9e5f6003a039faf37e3bd9c
tsekenrick <kt1556@nyu.edu>
2018-09-11 03:13:47 -0400

    basic user input in main program and first few methods

--- birthday.py
+++ birthday.py
@@ -1,1 +1,10 @@
 # birthday.py
+import mydate
+
+def main():
+    runCount = input("How many times should I run the simulation? ")
+    bdayCount = input("How many birthdays should I generate per trial? ")
+    print(mydate.date_to_string([1979, 10, 7]))
+    
+if __name__ == "__main__":
+    main()
--- mydate.py
+++ mydate.py
@@ -1,1 +1,13 @@
 # mydate.py
+def is_valid_month_num(n):
+    return n > 0 and n <= 12
+
+def month_num_to_string(month_num):
+    month_list = ['January', 'February', 'March', 'April', 'May', 'June',
+                  'July', 'August', 'September', 'October', 'November', 'December']
+    
+    return month_list[month_num - 1]
+
+def date_to_string(date_list):
+    return "{1} {2}, {0}".format(date_list[0], month_num_to_string(date_list[1]),
+            date_list[2])
