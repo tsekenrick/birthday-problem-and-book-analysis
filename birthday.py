df8dc2281a75424442ff60f98ec024530405ce4f
tsekenrick <kt1556@nyu.edu>
2018-09-11 06:09:41 -0400

    added some comments for my sanity

--- birthday.py
+++ birthday.py
@@ -13,22 +13,16 @@ def main():
     trials_with_rpt = 0
     for i in range(int(run_count)):
         
-        bdays = []
-        if len(bdays) != 0: bdays.clear() 
-        
+        bdays = []        
         for j in range(int(bday_count)):
-             bdays.append(mydate.generate_date(1997, 2017))
-        
-        bdays = mydate.remove_years(bdays)
-             
-        # bdays = [[10,1], [10,1], [3,31], [10,1], [3,31], [2, 21], [1, 5]]
-        bdays = mydate.dates_to_strings_no_year(bdays)
+             bdays.append(mydate.generate_date(1997, 2017))        
+        bdays = mydate.remove_years(bdays) # remove years
+        bdays = mydate.dates_to_strings_no_year(bdays) # convert to string
         unique_dates = set(bdays) # removes repeats from bdays
         
-        repeat_day_list = []
-        if len(repeat_day_list) != 0: repeat_day_list.clear()
         # loop compares bdays to unique_dates to create a list of 
         # the dates of same-day birthdays
+        repeat_day_list = []
         for days in unique_dates:
             if(bdays.count(days) > 1): repeat_day_list.append(days)            
         repeat_count = len(repeat_day_list)
